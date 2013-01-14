c39d0eb92bcdc798659a7da3c4b0694fa5901d0e
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-01-14 15:18:28 +0100

    codereview fixes a7da76f and cleaned up code for create_log method.

--- logger/tests/utils_tests.py
+++ logger/tests/utils_tests.py
@@ -1,21 +1,25 @@
 """Tests for the Utilities of the ``logger`` app."""
 from django.test import TestCase
 
-from logger.models import Log
+from logger.models import ActionParameter, Log
 from logger.utils import Logger
 
 
 class LoggerTestCase(TestCase):
     """Tests for the ``Logger`` class."""
+    longMessage = True
+
+    def setUp(self):
+        self.data = {'test_type1': 'val1', 'test_type2': 'val2'}
+
     def test_creates_log(self):
         """Test if ``create_log`` creates logs."""
         logger = Logger()
-        logger.create_log('test_action', 'test_action_type', 'test_value')
+        logger.create_log('test_action', self.data)
 
         self.assertEqual(Log.objects.all().count(), 1, msg=(
-            'When ``create_log`` is called, there should be one log in the'
-            ' database.'))
-        with self.assertRaises(IndexError):
-            logger.create_log('test_action', ['foo', 'bar'], 'value')
-        with self.assertRaises(IndexError):
-            logger.create_log('test_action', 'foo', ['value1', 'value2'])
+            '``create_log`` created the wrong amount of logs'))
+        self.assertEqual(
+            ActionParameter.objects.all().count(), len(self.data), msg=(
+                '``create_log`` created the wrong amount of action parameters.'
+            ))
--- logger/utils.py
+++ logger/utils.py
@@ -9,32 +9,26 @@ from logger.models import (
 
 class Logger(object):
     """The class that holds all the methods needed for logging."""
-    def create_log(self, action_name, parameter_type_names, values):
+    def create_log(self, action_name, param_dict):
         """
         Creates a ``Log`` object based on the above values.
 
         :action_name: String representing the action type. E.g. "payment"
-        :parameter_type_name: A list of strings or single string
-            representing the types of stored values.
-            E.g. ["amount",  "user"] or just "has used this"
-        :value: A list of values or a single value that is stored.
-            Each item in the list or the value itself can be either of the
-            following types: string, integer, date or datetime, decimal,
-            boolean or django model instance.
+        :param_dict: A dictionary in the format of
+            {'parameter_type1': 'value1', ... } defining the values of the
+            action to log.
 
         """
         # retrieve the action type
         action = self.get_action(action_name)
-        # retrieve the parameter list
-        action_parameters = self.get_action_parameters(
-            parameter_type_names, values)
 
         # create a new log
         log = Log(action=action)
         log.save()
+
         # assign the paremeters to the log
-        for parameter in action_parameters:
-            log.action_parameter.add(parameter)
+        for (param, value) in param_dict.iteritems():
+            log.action_parameter.add(self.get_action_parameter(param, value))
 
         return log
 
@@ -47,47 +41,21 @@ class Logger(object):
             action.save()
         return action
 
-    def get_action_parameters(self, parameter_type_names, values):
-        """Returns a list of ``ActionParameter`` objects."""
-        # if no lists were given, turn them into a list to make them iterable
-        if not isinstance(parameter_type_names, list):
-            parameter_type_name_list = [parameter_type_names]
-        else:
-            parameter_type_name_list = parameter_type_names
-        if not isinstance(values,  list):
-            value_list = [values]
-        else:
-            value_list = values
+    def get_action_parameter(self, param, value):
+        """Returns an ``ActionParameter`` object."""
+        # create  action parameter
+        action_parameter = ActionParameter(
+            parameter_type=self.get_parameter_type(param))
+        action_parameter.set_value(value)
+        action_parameter.save()
+        return action_parameter
 
-        # if the list lengths differ, raise an IndexError
-        if len(parameter_type_name_list) != len(value_list):
-            raise IndexError('The length of the given lists does not match.')
-
-        # iterate over parameter_type_names and create action parameters
-        parameter_type_list = self.get_parameter_types(
-            parameter_type_name_list, value_list)
-
-        # create list of action parameters
-        action_parameter_list = []
-        for (counter, parameter_type) in enumerate(parameter_type_list):
-            action_parameter = ActionParameter(parameter_type=parameter_type)
-            action_parameter.set_value(value_list[counter])
-            action_parameter.save()
-            action_parameter_list.append(action_parameter)
-        return action_parameter_list
-
-    def get_parameter_types(self, parameter_type_name_list, value_list):
-        """Retrieves a list of ``ActionParameterType`` objects."""
-        parameter_type_list = []
-        for parameter_type_name in parameter_type_name_list:
-            # retrieve ActionParameterType. If it doesn't exist, create it
-            # and append it to the list
-            try:
-                parameter_type = ActionParameterType.objects.get(
-                    name=parameter_type_name)
-            except ActionParameterType.DoesNotExist:
-                parameter_type = ActionParameterType(
-                    name=parameter_type_name)
-                parameter_type.save()
-            parameter_type_list.append(parameter_type)
-        return parameter_type_list
+    def get_parameter_type(self, param):
+        """Retrieves an ``ActionParameterType`` object."""
+        # retrieve ActionParameterType. If it doesn't exist, create it
+        try:
+            parameter_type = ActionParameterType.objects.get(name=param)
+        except ActionParameterType.DoesNotExist:
+            parameter_type = ActionParameterType(name=param)
+            parameter_type.save()
+        return parameter_type
